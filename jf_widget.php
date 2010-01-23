da62476ff4d6cc00f46d263fa86aadf32f1e49fa
Jehan <Jehan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-23 18:38:56 +0000

    All links title are changeable in the widget settings, and the changes are disabled if thepublication is disabled
    git-svn-id: https://plugins.svn.wordpress.org/jabber-feed/trunk@197170 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jf_widget.php
+++ jf_widget.php
@@ -47,7 +47,7 @@ class jabber_feed_widget extends WP_Widget
 	} // }}}
 
 	// This function deals with the actual display.
-	function widget ($args, $instance)
+	function widget ($args, $instance) // {{{
 	{
 		extract ($args);
 		$title = apply_filters('widget_title', $instance['title'] );
@@ -60,22 +60,28 @@ class jabber_feed_widget extends WP_Widget
 		echo '<ul>';
 		if (!empty ($configuration['publish_posts']))
 		{
+			$post_text = $instance['post_text'];
 			echo '<li>';
-			jabber_feed_display ($node = 'posts', $what = 'a', $text = 'TODO: Posts');
+			jabber_feed_display ($node = 'posts', $what = 'a', $text = $post_text);
 			echo '</li>';
 		}
 		if (!empty ($configuration['publish_comments']))
 		{
+			$comment_text = $instance['comment_text'];
 			echo '<li>';
-			jabber_feed_display ($node = 'comments', $what = 'a', $text = 'TODO: Comments');
+			jabber_feed_display ($node = 'comments', $what = 'a', $text = $comment_text);
 			echo '</li>';
-			if (is_singular ())
+			if (is_single ())
 			{
-				$id = get_the_ID ();
 				if (comments_open ($id) || count (get_approved_comments ($id) > 0))
 				{
+					$current_text = $instance['current_text'];
+					$id = get_the_ID ();
+					$current_post = get_post ($id);
+					$post_title = $current_post->post_title;
+					$current_text = strip_tags (str_ireplace ('[title]', $post_title, $current_text));
 					echo '<li>';
-					jabber_feed_display ($node = 'current', $what = 'a', $text = 'TODO: Comments of current post');
+					jabber_feed_display ($node = 'current', $what = 'a', $text = $current_text);
 					echo '</li>';
 				}
 			}
@@ -83,27 +89,54 @@ class jabber_feed_widget extends WP_Widget
 		echo '</ul>';
 
 		echo $after_widget;
-	}
+	} // }}}
 
-	function update ($new_instance, $old_instance)
+	function update ($new_instance, $old_instance) // {{{
 	{
 		$instance = $old_instance;
 
 		$instance['title'] = strip_tags ($new_instance['title']);
+		$instance['post_text'] = strip_tags ($new_instance['post_text']);
+		$instance['comment_text'] = strip_tags ($new_instance['comment_text']);
+		$instance['current_text'] = strip_tags ($new_instance['current_text']);
 
 		return $instance;
-	}
+	} // }}}
 
-	function form ($instance)
+	function form ($instance) // {{{
 	{
-		$defaults = array ('title' => 'Jabber feeds');
+		$configuration = get_option ('jabber_feed_configuration');
+		$defaults = array ('title' => 'Jabber feeds', 'post_text' => 'Entries (XMPP feed)',
+			'comment_text' => 'Comments (XMPP feed)',
+			'current_text' => "Comments of \"[title]\" (XMPP feed)");
 		$instance = wp_parse_args ((array) $instance, $defaults); ?>
 		<p>
 			<label for="<?php echo $this->get_field_id ('title'); ?>">Title:</label>
-			<input id="<?php echo $this->get_field_id ('title'); ?>" name="<?php echo $this->get_field_name ('title'); ?>" value="<?php echo $instance['title']; ?>" style="width:100%;" />
+			<input id="<?php echo $this->get_field_id ('title'); ?>" name="<?php echo $this->get_field_name ('title'); ?>" value='<?php echo $instance['title']; ?>' style="width:100%;" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id ('post_text'); ?>">Text for the link of posts feed:</label>
+			<input id="<?php echo $this->get_field_id ('post_text'); ?>" name="<?php echo $this->get_field_name ('post_text'); ?>" value='<?php echo $instance['post_text']; ?>' style="width:100%;" <?php
+			if (empty ($configuration['publish_posts']))
+				echo 'disabled="disabled"';
+		?>/>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id ('comment_text'); ?>">Text for the link of comments feed:</label>
+			<input id="<?php echo $this->get_field_id ('comment_text'); ?>" name="<?php echo $this->get_field_name ('comment_text'); ?>" value='<?php echo $instance['comment_text']; ?>' style="width:100%;" <?php 
+			if (empty ($configuration['publish_comments']))
+				echo 'disabled="disabled"';
+			?>/>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id ('current_text'); ?>">Text for the link of comments of current post feed (<em>[title]</em> will be replaced by the title of the post):</label>
+			<input id="<?php echo $this->get_field_id ('current_text'); ?>" name="<?php echo $this->get_field_name ('current_text'); ?>" value='<?php echo $instance['current_text']; ?>' style="width:100%;" <?php
+			if (empty ($configuration['publish_comments']))
+				echo 'disabled="disabled"';
+			?>/>
 		</p>
 <?php
-	}
+	} // }}}
 
 }
 
